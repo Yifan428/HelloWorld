ec4f21c8e70d772084496f5be0066f81775ca5a5
Shi Yifan <shiyifan@Shis-MacBook-Pro.local>
2017-08-28 21:06:38 -0500

    Finished Hello World Challenge!

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var infoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func helloSelected(_ sender: UIButton) {
+        infoLabel.text = "Hello World!"
+    }
+    @IBAction func clearSelected(_ sender: UIButton) {
+        infoLabel.text = ""
+    }
+    
 
 }
 
